0ddda1516e1592a2b2dc5281096dd7634d7742c8
4gungsetiabudi <4gungsetiabudi@gmail.com>
2024-01-26 21:36:41 +0700

    Pos Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('Master.Layouts.app', ['title' => $title])
+
+@section('content')>
+<style>
+    body {
+      background-image: url({{url('assets/default/maaf.png')}});
+    }
+    </style>
+</div>
+@endsection
